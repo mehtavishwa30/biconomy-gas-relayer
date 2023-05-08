3cb8da86b88eaed91b9c25177c3754f779e94903
“vish” <vishwa.mehta30@gmail.com>
2023-05-08 23:51:52 +0530

    add token addresses

--- biconomy-gas-relayer/src/components/Auth.tsx
+++ biconomy-gas-relayer/src/components/Auth.tsx
@@ -5,3 +5,32 @@ import { ChainId } from '@biconomy/core-types'
 import { ethers } from 'ethers'
 import SmartAccount from '@biconomy/smart-account'
 import { css } from '@emotion/css'
+
+const tokens = [
+    {
+      address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+      decimals: 6,
+      symbol: 'USDC'
+    },
+    {
+      address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+      decimals: 6,
+      symbol: 'USDT'
+    },
+    {
+      address: '0x0000000000000000000000000000000000001010',
+      decimals: 18,
+      symbol: 'MATIC',
+    },
+    {
+      address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+      decimals: 18,
+      symbol: 'DAI'
+    },
+    {
+      address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+      decimals: 18,
+      symbol: 'WETH'
+    }
+  ]
+  
